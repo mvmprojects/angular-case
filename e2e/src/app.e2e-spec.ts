e6f6b5c8806be9cc2f03fd6aeeb4c65f7a8125e3
nestedforloop <nestedforloop@users.noreply.github.com>
2020-03-24 22:37:14 +0100

    working on protractor e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,10 +8,23 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   expect(page.getTitleText()).toEqual('case-app app is running!');
+  // });
+
+  it('should have access to input field of app-search-bar', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('case-app app is running!');
-  });
+    expect(element(by.css('app-search-bar input')).isPresent());
+  });  
+
+  it('should enter the letter \'e\' and choose first option', () => {
+    page.selectArtistInDropdown();
+  } );
+
+  it('should be able to click first album in the list', () => {
+    page.selectAlbum();
+  } );  
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
